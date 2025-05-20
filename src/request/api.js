e0d110a02cdf073165fcca06ad92c1c408b190d2
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2025-05-20 10:10:20 +0800

    Update api.js

--- src/request/api.js
+++ src/request/api.js
@@ -12,4 +12,9 @@ export const getTotalScanNum = data => get("v1/total_scan_num", data);
 
 //消耗CPU
 export const consumeCpu = data => get("v1/consume_cpu", data);
- 
+
+// 新增订单队列API
+import {get} from "./axios"
+
+// 获取买卖订单队列
+export const getOrderQueue = (params) => get("v1/order_queue", params)
