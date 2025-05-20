cf3d97a55e68e6aecedbe13d86df7fcc63c53ebb
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2025-05-20 10:07:45 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue';
-import ElementPlus from 'element-plus';
-import '../node_modules/element-plus/dist/index.css'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createI18n } from 'vue-i18n'
+import zh from './locales/zh.json'
+import en from './locales/en.json'
+
+const i18n = createI18n({
+  locale: 'zh',
+  fallbackLocale: 'en',
+  messages: { zh, en }
+})
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(i18n)
 app.mount('#app')
-
-
