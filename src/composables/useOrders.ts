29a838d57ad4be6416ec8b78b2c16afce41e1406
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2025-05-29 17:28:28 +0800

    Update useOrders.ts

--- src/composables/useOrders.ts
+++ src/composables/useOrders.ts
@@ -1,7 +1,7 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import axios from 'axios'
 
-const PROVIDER_BASE_URL = 'http://80.238.230.117:80'
+const PROVIDER_BASE_URL = 'http://cae.demo.huawei.com'
 const mockData = [
   { id: '100001', time: '2025-05-20 17:00:00', quantity: 10, price: 100.5, status: 'NEW', type: 'MOCK' },
   { id: '100002', time: '2025-05-20 17:00:01', quantity: 5, price: 101, status: 'FILLED', type: 'MOCK'},
